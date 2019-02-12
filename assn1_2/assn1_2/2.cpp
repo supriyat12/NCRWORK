81572a9d9859a38e5f300f189b1c3895b643f1ec
tanishkave <tanishka.ve@gmail.com>
2019-02-12 15:15:35 -0800

    added aasn1_2

---
+++ assn1_2/assn1_2/2.cpp
@@ -0,0 +1,62 @@
+#include<stdio.h>
+void main()
+{
+	int X, P, N, RESULT;
+	int invert(int, int, int);
+
+	printf("Enter an integer : \n");
+	scanf_s("%d", &X);
+	printf("enter start position\n");
+	scanf_s("%d", &P);
+	printf("enter the length\n");
+	scanf_s("%d", &N);
+	printf("converted binary value is", X);
+	RESULT = invert(X, P, N);
+	printf("in decimal form %d", RESULT);
+	//getch();
+}
+
+int invert(int X, int P, int N)
+{
+	int I, Y, LIMIT, LEN, BIN[15];
+	LEN = 0;
+	while (X>0)
+	{
+		BIN[LEN] = X % 2;
+		X = X / 2;
+		LEN++;
+	};
+
+	for (I = LEN - 1; I>-1; I--)
+	{
+		printf("%d ", BIN[I]);
+	}
+	printf("\n");
+	LIMIT = P + N;
+	if (LIMIT < LEN)
+	{
+		for (I = P - 1; I<P + N - 1; I++)
+		{
+			if (BIN[I] == 0)
+				BIN[I] = 1;
+			else
+				BIN[I] = 0;
+		}
+	}
+	else
+	{
+		printf("lrge value entered for p an n");
+	}
+
+	printf("inverted integer is\t");
+	for (I = LEN - 1; I>-1; I--)
+	{
+		printf("%d ", BIN[I]);
+	}
+	Y = 0;
+	for (I = LEN - 1; I >= 0; I--)
+	{
+		Y = Y * 2 + BIN[I];
+	}
+	return Y;
+}
